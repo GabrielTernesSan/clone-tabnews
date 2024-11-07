639def471e0f6572085f6d8770e75f950e44971d
Gabriel Ternes <73451261+GabrielTernesSan@users.noreply.github.com>
2024-11-07 17:48:46 +0000

    Correção

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>As mais impressionantes façanhas humanas são, na realidade, o resultado do agregado de inúmeros elementos isolados, cada um dos quais, em certo sentido, nada tem de extraordinário.</h1>
+}
+
+export default Home;
